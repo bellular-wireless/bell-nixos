d3270a7def7e261c712265bbe187ec63daef5d15
Bell Dixon <bell.dixon@proton.me>
2025-06-17 00:38:23 -0500

    move mounts to modules folder will need to modularize a bit more in the future

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./mounts.nix
+      ./modules/mounts.nix
     ];
 
   services.xserver.videoDrivers = ["nvidia"];
